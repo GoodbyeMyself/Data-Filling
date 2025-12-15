a89120894360f6a3483c9e846668124dadb97fd1
GoodbyeMyself <15500409521@163.com>
2025-12-15 11:32:16 +0800

    重定向更正：  补充测试参数，便于测试

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,7 +6,18 @@ const routes: Array<RouteRecordRaw> = [
         path: "/",
         name: "root",
         component: Layout,
-        redirect: { name: "Demo" },
+        redirect: () => ({
+            name: "Filling",
+            /**
+             * @description: 测试参数
+             * @author: M.yunlong
+             * @date: 2025-12-15 11:20:54
+            */
+            query: {
+                from: "home",
+                ts: "1765768717757"
+            }
+        }),
         children: [
             {
                 path: "test",
@@ -21,7 +32,8 @@ const routes: Array<RouteRecordRaw> = [
                 name: "Filling",
                 component: () => import("@/views/filling/index.vue"),
                 meta: {
-                    title: "数据填报"
+                    title: "数据填报",
+                    hideThemeToggle: true
                 }
             },
             {
