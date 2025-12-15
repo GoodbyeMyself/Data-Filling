e0afd31e47b09ee64cd47ba7cbacc667523799e6
GoodbyeMyself <15500409521@163.com>
2025-12-15 11:31:44 +0800

    根据路由判断 :  是否 显示 主题色切换

--- src/components/nav-bar/index.vue
+++ src/components/nav-bar/index.vue
@@ -3,16 +3,29 @@ import {
   useDarkMode,
   useToggleDarkMode
 } from "@/composables/useToggleDarkMode";
+import { useRoute } from "vue-router";
+import { computed } from "vue";
+
+const route = useRoute();
 
 const onClickRight = (event: TouchEvent | MouseEvent) => {
   useToggleDarkMode(event);
 };
+
+// 根据路由 meta 判断是否隐藏主题切换按钮
+const hideThemeToggle = computed(() => {
+  return route.meta?.hideThemeToggle === true;
+});
 </script>
 
 <template>
   <van-nav-bar fixed placeholder @click-right="onClickRight">
     <template #right>
-      <svg-icon class="text-[18px]" :name="useDarkMode() ? 'light' : 'dark'" />
+      <svg-icon 
+        v-if="!hideThemeToggle" 
+        class="text-[18px]" 
+        :name="useDarkMode() ? 'light' : 'dark'" 
+      />
     </template>
   </van-nav-bar>
 </template>
